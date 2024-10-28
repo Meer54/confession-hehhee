383ef91aa549bf74f698d5b8c13277f69bec69d6
Meer54 <139063530+Meer54@users.noreply.github.com>
2024-10-28 19:54:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-const wrapper = document.querySelector(".wrapper");
 const question = document.querySelector(".question");
 const gif = document.querySelector(".gif");
 const yesBtn = document.querySelector(".yes-btn");
@@ -6,7 +5,7 @@ const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
   question.innerHTML = "Yay, I like you too!!";
-  gif.src = "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
+  gif.style.display = "block"; // Display the GIF when 'Yes' is clicked
 });
 
 noBtn.addEventListener("mouseover", () => {
